cbe691ed3fd53896fe9f62a92d23d441a1844597
Shubham Parashar <shubhamparasharjind@gmail.com>
2024-08-06 15:16:17 +0530

    done some seo

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { hydrate, render } from 'react-dom';
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes";
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+
+const App = () => (
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>,
+// )
+
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+}
+else {
+  render(<App />, rootElement);
+}
