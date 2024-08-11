d79e306c59f21f0651c5df63175d7820ae8a5ab4
Shubham Parashar <shubhamparasharjind@gmail.com>
2024-08-12 00:52:56 +0530

    lazy loaded components

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,22 +1,24 @@
+import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import Countries from "./Components/countries/Countries";
-import EnquireNow from "./Components/EnquireNow/EnquireNow";
-import PlaceEnquire from "./Components/EnquireNow/PlaceEnquire";
-import AboutUs from "./Components/AboutUs/AboutUs";
-import ContactUs from "./Components/Contact/ContactUs";
-import ServicesPage from "./Components/Services/ServicesPage";
-import USA from "./Components/countries/USA";
-import Europe from "./Components/countries/Europe";
-import India from "./Components/countries/India";
-import V1 from "./Components/Services/V1";
-import V2 from "./Components/Services/V2";
-import V3 from "./Components/Services/V3";
-import V4 from "./Components/Services/V4";
-import V5 from "./Components/Services/V5";
-import V6 from "./Components/Services/V6";
-import TAC from "./Components/T&C/TAC";
+const Countries = lazy(() => import("./Components/countries/Countries"));
+const EnquireNow = lazy(() => import("./Components/EnquireNow/EnquireNow"));
+const PlaceEnquire = lazy(() => import("./Components/EnquireNow/PlaceEnquire"));
+const AboutUs = lazy(() => import("./Components/AboutUs/AboutUs"));
+const ContactUs = lazy(() => import("./Components/Contact/ContactUs"));
+const ServicesPage = lazy(() => import("./Components/Services/ServicesPage"));
+const USA = lazy(() => import("./Components/countries/USA"));
+const Europe = lazy(() => import("./Components/countries/Europe"));
+const India = lazy(() => import("./Components/countries/India"));
+const V1 = lazy(() => import("./Components/Services/V1"));
+const V2 = lazy(() => import("./Components/Services/V2"));
+const V3 = lazy(() => import("./Components/Services/V3"));
+const V4 = lazy(() => import("./Components/Services/V4"));
+const V5 = lazy(() => import("./Components/Services/V5"));
+const V6 = lazy(() => import("./Components/Services/V6"));
+const TAC = lazy(() => import("./Components/T&C/TAC"));
 // import Global from "./Components/countries/Global";
+
 
 const router = createBrowserRouter([
   {
@@ -25,94 +27,171 @@ const router = createBrowserRouter([
   },
   {
     path: "/AboutUs",
-    element: <AboutUs />,
+    element: (
+      <Suspense fallback={<h3>Content Loading...</h3>}>
+        <AboutUs />,
+      </Suspense>
+    )
   },
   {
     path: "/Services",
-    element: <ServicesPage />,
+    element: (
+      <Suspense fallback={<h3>Content Loading...</h3>}>
+        <ServicesPage />,
+      </Suspense>
+    )
+
   },
   {
     path: "/ContactUs",
-    element: <ContactUs />,
+    element: (
+      <Suspense fallback={<h3>Content Loading...</h3>}>
+        <ContactUs />,
+      </Suspense>
+    )
+
   },
   {
     path: "/EnquireNow",
-    element: <EnquireNow />,
+    element: (
+      <Suspense fallback={<h3>Content Loading...</h3>}>
+        <EnquireNow />,
+      </Suspense>
+    )
+
   },
-  // {
-  //   path: "/Blog",
-  //   element: <BlogGrid />,
-  // },
+
   {
     path: "/EnquireNow/:id",
-    element: <PlaceEnquire />,
+    element: (
+      <Suspense fallback={<h3>Content Loading...</h3>}>
+        <PlaceEnquire />,
+      </Suspense>
+    )
+
   },
   {
     path: "/T&C",
-    element: <TAC />,
+    element: (
+      <Suspense fallback={<h3>Content Loading...</h3>}>
+        <TAC />,
+      </Suspense>
+    )
+
   },
 
   //////////Blogs Pages//////////////////
 
-  // {
-  //   path: "/Blog1",
-  //   element: <Blog1 />,
-  // },
+
 
   ////////////Countries Pages///////////////
 
   {
     path: "/countries",
-    element: <Countries />,
+    element: (
+      <Suspense fallback={<h3>Content Loading...</h3>}>
+        <Countries />,
+      </Suspense>
+    )
+
   },
   {
     path: "/USA",
-    element: <USA />,
+    element: (
+      <Suspense fallback={<h3>Content Loading...</h3>}>
+        <USA />,
+      </Suspense>
+    )
+
   },
   {
     path: "/Europe",
-    element: <Europe />,
+    element: (
+      <Suspense fallback={<h3>Content Loading...</h3>}>
+        <Europe />,
+      </Suspense>
+    )
+
   },
   {
     path: "/India",
-    element: <India />,
+    element: (
+      <Suspense fallback={<h3>Content Loading...</h3>}>
+        <India />,
+      </Suspense>
+    )
+
   },
   {
     path: "/Global",
-    element: <Countries />,
+    element: (
+      <Suspense fallback={<h3>Content Loading...</h3>}>
+        <Countries />,
+      </Suspense>
+    )
+
   },
   ////////////Services Pages///////////////
 
   {
     path: "/services/Join-Our-Academy",
-    element: <V1 />,
+    element: (
+      <Suspense fallback={<h3>Content Loading...</h3>}>
+        <V1 />,
+      </Suspense>
+    )
+
   },
 
   {
     path: "/services/Register-For-Tennis-&-Education",
-    element: <V2 />,
+    element: (
+      <Suspense fallback={<h3>Content Loading...</h3>}>
+        <V2 />,
+      </Suspense>
+    )
+
   },
 
   {
     path: "/services/Scholarship-in-USA-&-Europe",
-    element: <V3 />,
+    element: (
+      <Suspense fallback={<h3>Content Loading...</h3>}>
+        <V3 />,
+      </Suspense>
+    )
+
   },
 
   {
     path: "/services/Tennis-Camp-in-Spain",
-    element: <V4 />,
+    element: (
+      <Suspense fallback={<h3>Content Loading...</h3>}>
+        <V4 />,
+      </Suspense>
+    )
+
   },
 
   {
     path: "/services/Career-Counseling",
-    element: <V5 />,
+    element: (
+      <Suspense fallback={<h3>Content Loading...</h3>}>
+        <V5 />,
+      </Suspense>
+    )
+
   },
   {
     path: "/services/Book-a-course",
-    element: <V6 />,
+    element: (
+      <Suspense fallback={<h3>Content Loading...</h3>}>
+        <V6 />,
+      </Suspense>
+    )
+
   },
 ]);
-
 
 
 export default router;
