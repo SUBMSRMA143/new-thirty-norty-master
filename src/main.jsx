f3b2e85b3bd85b051a3620b03e30ae7824c7ffd9
Shubham Parashar <shubhamparasharjind@gmail.com>
2024-09-29 01:32:53 +0530

    trial 6

--- src/main.jsx
+++ src/main.jsx
@@ -3,31 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes";
 import './index.css'
-import { Helmet } from 'react-helmet';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    <Helmet>
-      <script type="application/ld+json">
-        {`
-          {
-            "@content": "https://schema.org",
-            "@type": "Organization",
-            "name": "Thirty40",
-            "url": "https://new-thirty-forty-master.vercel.app/",
-            "logo": "https://new-thirty-forty-master.vercel.app/assets/logo-CxDQB7Xr.png",
-            "contactPoint": {
-                  "@type": "ContactPoint",
-                  "telephone": "+91-7892650335",
-                  "email": "info@thirty40.in",
-                  "areaServed": ["USA", "INDIA", "EUROPE"]
-                  "contactType": "Customer Service",
-          }`
-        }
-      </script>
-    </Helmet>
   </React.StrictMode>,
 )
 
